64eba2748f51b26e0cd4abaece9775a915eccf1c
Avneet Kahlon <n11672889@qut.edu.au>
2025-10-09 16:09:22 +1000

    renamed parent folder

--- KStage/forms.py
+++ KStage/forms.py
@@ -45,7 +45,7 @@ class EventForm(FlaskForm):
     location = StringField('Venue', validators=[InputRequired()])
     price = StringField('Ticket Price', validators=[InputRequired()])
     available = StringField('Ticket Available', validators=[InputRequired()])
-    image = StringField('Event Image', validators=[InputRequired()]
+    image = StringField('Event Image', validators=[InputRequired()])
     description = TextAreaField('Description', validators = [InputRequired()])
 
     #submit button
@@ -56,4 +56,3 @@ class EventForm(FlaskForm):
 class CommentForm(FlaskForm):
     comment = TextAreaField("Add a Comment", validators=[DataRequired(), Length(min=1, max=500)])
     submit = SubmitField("Post Comment")
-
