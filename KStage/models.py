f5020a07e3a3b37ff30a847a062df55e1cccd925
Avneet Kahlon <n11672889@qut.edu.au>
2025-10-22 01:03:44 +1000

    updated models.py

--- KStage/models.py
+++ KStage/models.py
@@ -4,16 +4,26 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), unique=True, nullable=False)
-    surname = db.Column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    surname = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True)
+    phone_number = db.Column(db.String(20))
+    street_address = db.Column(db.String(200))
     password_hash = db.Column(db.String(255), nullable=False)
     comments = db.relationship('Comment', backref='user')
 
-    def __repr__(self):
-        return f"Name: {self.name}"
+    def set_password(self, password):
+        """Hashes password before storing"""
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        """Validates input password against stored hash."""
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f"<User {self.first_name} {self.surname}>"
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
