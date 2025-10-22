6192988a11b1cfc954494695b7013c10a610fbc5
swaaa2305 <svathi2008@gmail.com>
2025-10-22 12:48:17 +1000

    check

--- KStage/event.py
+++ KStage/event.py
@@ -20,9 +20,9 @@ def create():
 
 def get_event():
     #create description of BTS fan signing event
-    b_desc = """"Get ready for the ultimate ARMY experience! Join us for an unforgettable BTS Fan Signing Event, where you’ll have the rare opportunity to meet your favorite members of BTS up close. This exclusive event brings fans together to celebrate love, music, and connection — the true essence of BTS. """
+    #b_desc = """"Get ready for the ultimate ARMY experience! Join us for an unforgettable BTS Fan Signing Event, where you’ll have the rare opportunity to meet your favorite members of BTS up close. This exclusive event brings fans together to celebrate love, music, and connection — the true essence of BTS. """
    
-    event = Event ('BTS fan sign', b_desc, 'Thursday, 9th October 2025', '7:00pm', 'Starting from $2000', 'Approx. 3 hours')
+    #event = Event ('BTS fan sign', b_desc, 'Thursday, 9th October 2025', '7:00pm', 'Starting from $2000', 'Approx. 3 hours')
 
     #feature_section = feature_section("Experience all of TWICE's biggest hits including 'TT', 'Likey', 'What Is Love', and tracks from their latest albums.", "State-of-the-art lighting, LED screens, and stunning stage effects that create an immersive concert experience.", 'Special moments with ONCE, fan chants, and exclusive merchandise available only at the concert venue.')
     #feature_section = feature_section("Sam", "Visited during the olympics, was great", '2023-08-12 11:00:00')
@@ -34,4 +34,4 @@ def get_event():
     #feature_section = feature_section("Sally", "free face masks!", '2023-08-12 11:00:00')
     #event.set_feature_section(feature_section)
     
-    return event
+    return "event"
