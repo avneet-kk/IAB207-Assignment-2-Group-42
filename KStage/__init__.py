9d50f7db0c09f1e4084d2b6971ff5ceb5dbffb13
swaaa2305 <svathi2008@gmail.com>
2025-10-23 15:23:30 +1000

    called user twice

--- KStage/__init__.py
+++ KStage/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .models import User
+
 
 db = SQLAlchemy()
 
