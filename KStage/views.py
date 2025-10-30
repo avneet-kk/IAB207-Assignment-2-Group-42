40dc2a499ded8fdcbcedafc2f66ecea3f14f016c
swaaa2305 <svathi2008@gmail.com>
2025-10-30 20:04:24 +1000

    navbar

--- KStage/views.py
+++ KStage/views.py
@@ -56,6 +56,12 @@ def event_detail(event_id: int):
     # GET or invalid POST -> render page with form + event details
     return render_template('event_details.html', event=event, form=form)
 
+@main_bp.route('/create_events')
+def create_events():
+    """List all events from the database."""
+    # create_event = Event.query.order_by(Event.date.asc()).all()
+    return render_template('create-event.html')#, create_events=create_events)
+
 @main_bp.route('/bookings')
 @login_required
 def booking_history():
