c9ccb65d29d17ae5a2daca0956a24e24944baa42
swaaa2305 <svathi2008@gmail.com>
2025-11-02 15:33:47 +1000

    remove unwanted comments

--- KStage/event.py
+++ KStage/event.py
@@ -13,10 +13,8 @@ print('blueprint completed')
 @event_destbp.route('/create', methods = ['GET', 'POST'])
 # @login_required
 def createbp():
-  print('function working inside')
   print('Method type: ', request.method)
   form = EventForm()
-  print('passed section A')
   if form.validate_on_submit():
 
     event = Event(title=form.title.data, 
@@ -47,25 +45,15 @@ def createbp():
         for field, errors in form.errors.items():
             for error in errors:
                 flash(f'{field}: {error}', 'danger')
-  print('created event inside')
   return render_template('destination/create.html', form=form)
-
-
-print('created event outside')
 
 @event_destbp.route ('/<id>') 
 def show (id):
-    print('passed show function')
     Event = db.session.scalar(db.select(Event).where(Event.id==id))
-    print('connection sucessful')
-
     # Event = get_event ()
     return render_template ('destination/show.html', event = Event)
-print('complete showed')
 
-
-
-
+#function to upload file
 def check_upload_file(form):
   # get file data from form  
   fp = form.image.data
@@ -79,36 +67,3 @@ def check_upload_file(form):
   # save the file and return the db upload path  
   fp.save(upload_path)
   return db_upload_path
-
-  
-
-    # return redirect(url_for('events'))
-  
-  # Redirect to the event's detail page after successful creation
-  # return redirect(url_for('main.event_detail', event_id=new_event.id))
-
-def get_event():
-    title = """BTS 6th World Tour"""
-    category = """Concert"""
-    description="""It's that time of year ARMY's... BTS is back with another World Tour!"""
-    date='30th October, 2025 | 7:00pm'
-    location="""Suncorp Stadium"""
-    total_tickets='8000'
-    ticket_price='1500'
-    image_path='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFyC8pBJI2AAHLpAVih41_yWx2xxLleTtdshAdk1HOZQd9ZM8-Ag'
-    return Event
-
-  # class Event(db.Model):
-  #   id = db.Column(db.Integer, primary_key=True)
-  #   title = db.Column(db.String(160), nullable=False)
-  #   category = db.Column(db.String(80), nullable=False)
-  #   description = db.Column(db.Text, nullable=False)
-  #   date = db.Column(db.DateTime, nullable=False)
-  #   location = db.Column(db.String(160), nullable=False)
-  #   total_tickets = db.Column(db.Integer, default=0, nullable=False)
-  #   sold_tickets  = db.Column(db.Integer, default=0, nullable=False)
-  #   is_cancelled  = db.Boolean,
-  #   image_path    = db.Column(db.String(255))
-  #   owner_id      = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
